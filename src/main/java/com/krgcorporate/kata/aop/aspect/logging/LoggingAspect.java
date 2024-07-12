abc3306a29deba698d3561f214795b9c5890aded
gerard puig <gerardpuig31@hotmail.com>
2024-07-12 18:52:57 +0200

    Test 3 and 4: Improved with common @PointCut

--- src/main/java/com/krgcorporate/kata/aop/aspect/logging/LoggingAspect.java
+++ src/main/java/com/krgcorporate/kata/aop/aspect/logging/LoggingAspect.java
@@ -5,18 +5,23 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Slf4j
 @Aspect
 public class LoggingAspect {
 
-    @After("bean(*Service)")
+    @Pointcut("bean(*Service)")
+    public void pointCut() {
+    }
+
+    @After("pointCut()")
     public void logAfter(JoinPoint jp) {
         log.info("After calling {}", jp.getSignature());
     }
 
     @AfterThrowing(
-            pointcut="bean(*Service)",
+            pointcut="pointCut()",
             throwing="throwable")
     public void logAfterThrowing(Throwable throwable) {
         log.error("Unhandled exception", throwable);
