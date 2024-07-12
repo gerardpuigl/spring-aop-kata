ea041aa8da439fbbb7806e043957761ec53d6df8
gerard puig <gerardpuig31@hotmail.com>
2024-07-12 18:46:35 +0200

    Test 3: Any method in a bean with Service as a suffix

--- src/main/java/com/krgcorporate/kata/aop/aspect/logging/LoggingAspect.java
+++ src/main/java/com/krgcorporate/kata/aop/aspect/logging/LoggingAspect.java
@@ -2,13 +2,18 @@ package com.krgcorporate.kata.aop.aspect.logging;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
 
 @Slf4j
+@Aspect
 public class LoggingAspect {
 
+    @After("bean(*Service)")
     public void logAfter(JoinPoint jp) {
         log.info("After calling {}", jp.getSignature());
     }
+
 
     public void logAfterThrowing(Throwable throwable) {
         log.error("Unhandled exception", throwable);
