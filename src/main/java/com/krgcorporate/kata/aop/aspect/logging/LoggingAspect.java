25ae5929d206c413d2ead7b6d5e7b4b4741fc662
gerard puig <gerardpuig31@hotmail.com>
2024-07-12 18:50:52 +0200

    Test 4: Any method in a bean with Service as a suffix that Throws an exception

--- src/main/java/com/krgcorporate/kata/aop/aspect/logging/LoggingAspect.java
+++ src/main/java/com/krgcorporate/kata/aop/aspect/logging/LoggingAspect.java
@@ -3,6 +3,7 @@ package com.krgcorporate.kata.aop.aspect.logging;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 
 @Slf4j
@@ -14,7 +15,9 @@ public class LoggingAspect {
         log.info("After calling {}", jp.getSignature());
     }
 
-
+    @AfterThrowing(
+            pointcut="bean(*Service)",
+            throwing="throwable")
     public void logAfterThrowing(Throwable throwable) {
         log.error("Unhandled exception", throwable);
     }
