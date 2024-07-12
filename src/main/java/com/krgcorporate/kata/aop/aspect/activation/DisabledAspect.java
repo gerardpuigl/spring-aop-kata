1fcc1ddb22a4c2e98e0da923a6cca5dbdfa8767a
gerard puig <gerardpuig31@hotmail.com>
2024-07-12 18:38:01 +0200

    Test 1: solve before method with specific annotation

--- src/main/java/com/krgcorporate/kata/aop/aspect/activation/DisabledAspect.java
+++ src/main/java/com/krgcorporate/kata/aop/aspect/activation/DisabledAspect.java
@@ -1,7 +1,12 @@
 package com.krgcorporate.kata.aop.aspect.activation;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
 public class DisabledAspect {
 
+    @Before("@annotation(disabled)")
     public void beforeDisabledMethod(Disabled disabled) throws DisabledException {
         throw new DisabledException();
     }
