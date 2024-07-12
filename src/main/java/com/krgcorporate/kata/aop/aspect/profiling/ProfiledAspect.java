9dc5262a22ed12fe37e4afdb755decc4b08c5ad5
gerard puig <gerardpuig31@hotmail.com>
2024-07-12 18:41:57 +0200

    Test 2: Around method with annotation in the method

--- src/main/java/com/krgcorporate/kata/aop/aspect/profiling/ProfiledAspect.java
+++ src/main/java/com/krgcorporate/kata/aop/aspect/profiling/ProfiledAspect.java
@@ -3,13 +3,17 @@ package com.krgcorporate.kata.aop.aspect.profiling;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 
 @Slf4j
 @AllArgsConstructor
+@Aspect
 public class ProfiledAspect {
 
     private final Profiler profiler;
 
+    @Around("@annotation(Profiled)")
     public Object aroundProfiledMethods(ProceedingJoinPoint pjp) throws Throwable {
         return profiler.profile(pjp.getSignature().getName(), pjp::proceed);
     }
